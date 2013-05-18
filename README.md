7cf393ef6cd86f2e8c264904e0392864d81987ee
aymanim <me@ayman.im>
2013-05-17 20:22:07 -0400

    Trivial changes

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ xtrc
 ====
 
 Run all cores at close to 100% for some amount of time (not clearly defined)
+
+Uses multiple "transfer chains" made of a sender [S], relay(s) [R] and reciever [C]
+
+Sender initates a transfer of trivial data over a pipe, and the data travels
+the entire S-R..-C chain. 
+
+Program stopps after all the Cs have received the data
--- stress_all.py
+++ stress_all.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import multiprocessing
 
 
@@ -12,6 +11,7 @@ def sender(conn, iters, comms):
             conn.send(str(i))
     conn.close()
 
+
 def relay(conn1, conn2, iters, comms):
     for i in range(iters):
         for c in range(comms):
@@ -20,7 +20,8 @@ def relay(conn1, conn2, iters, comms):
     conn1.close()
     conn2.close()
 
-def reciever(conn, iters, comms):
+
+def receiver(conn, iters, comms):
     for i in range(iters):
         for c in range(comms):
             # print 
@@ -31,7 +32,7 @@ def reciever(conn, iters, comms):
 if __name__ == '__main__':
     num_relays = 3
     num_iterations = 100000
-    num_comms_per_iteration = 5
+    num_comms_per_iteration = 5 
     num_cores = multiprocessing.cpu_count()
     num_transfers = num_cores
     print num_transfers
@@ -46,31 +47,30 @@ if __name__ == '__main__':
 
     print len(pipes)
 
-    # Each core needs a sender
+    # Each transfer chain needs at least one sender
     for i in range(num_transfers):
         transfers.append([multiprocessing.Process(target=sender, args=(pipes[i * (num_relays+1)][0], 
             num_iterations, num_comms_per_iteration))])
 
-    # Set up the relays
+    # Setup the relays
     for i in range(num_transfers):
         for r in range(num_relays):
             transfers[i].append(multiprocessing.Process(target=relay, args=(pipes[i * (num_relays+1) + r][1], 
                 pipes[i * (num_relays+1) + r + 1][0], num_iterations, num_comms_per_iteration)))
 
-    # Each the transfer with a reception
+    # Each transfer chain ends with a receiver
     for i in range(num_transfers):
-        transfers[i].append(multiprocessing.Process(target=reciever, args=(pipes[i * (num_relays+1) - 1][1], num_iterations, 
+        transfers[i].append(multiprocessing.Process(target=receiver, args=(pipes[i * (num_relays+1) - 1][1], num_iterations, 
             num_comms_per_iteration)))
-
 
     for t in transfers:
         for process in t:
             process.start()
-
     
     for t in transfers:
         for process in t:
             process.join()
+
 
 
 
